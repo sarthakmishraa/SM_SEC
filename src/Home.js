2cda7fe3984dd481839a0edad8bc767dc22dbead
Sarthak Mishra <56118819+sarthakmishraa@users.noreply.github.com>
2024-02-16 21:07:26 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,7 @@
 // Home.js
-import React, { useState, useRef } from "react";
-import UploadSection from './UploadSection';
-import VideoPlayer from './VideoPlayer';
-import RealTimeDetection from './RealTimeDetection';
+import React from "react";
+// import VideoPlayer from './VideoPlayer';
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
 
 function Home() {
@@ -11,10 +10,21 @@ function Home() {
         <h1>SM Security</h1>
         <p>Welcome to SM Sec, a software which gives you video surveillance options.
           Currently this is underconstruction</p>
+
+        <div className="HomeFeature">
+          <h4>Real Time Detection using COCOSSD </h4>
+          <Link to={"/liveDetect"}>
+            <button>Click</button>
+          </Link>
+        </div>
+        <div className="HomeFeature">
+          <h4>Image Object Detection using COCOSSD </h4>
+          <Link to={"/imgDetect"}>
+            <button>Click</button>
+          </Link>
+        </div>
         
-        <RealTimeDetection></RealTimeDetection>
-        <UploadSection></UploadSection>
-        <VideoPlayer></VideoPlayer>
+        {/* <VideoPlayer></VideoPlayer> */}
         <Footer></Footer>
     </div>
   );
