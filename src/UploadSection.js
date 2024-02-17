7ffc42f603f41486371e13b7d4f97ce880b9d723
Sarthak Mishra <56118819+sarthakmishraa@users.noreply.github.com>
2024-02-16 21:08:21 -0500

    Update UploadSection.js

--- src/UploadSection.js
+++ src/UploadSection.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl"
 import * as cocoSSD from "@tensorflow-models/coco-ssd";
+import Footer from './Footer';
 
 function UploadSection() {
 
   const [imgData, setImgData] = useState(null);
+  const [results, setResults] = useState([]);
 
   const readImage = (file) => {
     return new Promise((rs, rj) => {
@@ -23,33 +25,44 @@ function UploadSection() {
     setImgData(imgData);
   };
 
-  const handleUpload = async (imgData) => {
+  const handleUpload = async () => {
     const imgElement = document.createElement("img");
     imgElement.src = imgData;
 
     imgElement.onload = async () => {
-      console.log("inside image element onload");
-      await detectObjectFromImage(imgElement);
+      const result = await detectObjectFromImage(imgElement);
+      setResults(result);
     };
 
     imgElement.onerror = async (error) => {
       console.log("ERROR: ", error.message);
-    };    
+    };
   };
 
   const detectObjectFromImage = async (imgElement) => {
     const model = await cocoSSD.load();
     const predictions = await model.detect(imgElement, 5);
     console.log("Predictions: ", predictions);
+    return predictions;
   };
 
   return (
     <div className="UploadSection">
-      <div className="ImageContainer">
-        {imgData ? <img className="imageDetect" src={imgData} alt="Insert object here"></img> : <h3>Uploaded/detected Image will be shown here</h3>}
+      <div className="ImageDetection">
+        <h3>Upload an image for object detection</h3>
+        <div className="ImageContainer">
+          {imgData ? <img className="imageDetect" src={imgData} alt="Insert object here"></img> : <h3>Uploaded/detected Image will be shown here</h3>}
+        </div>
+        <br/>
+        <input type="file" onChange={ handleFileChange }></input>
+        <br/>
+        <button onClick={ handleUpload }>Detect</button>
+        <h3></h3>
+        <h3>
+        No of classes detected: {results.length} <br/> {results.map(detection => <div>{detection.class} {(detection.score).toFixed(2)*100}%</div>)}
+        </h3>
       </div>
-      <input type="file" onChange={ handleFileChange }></input>
-      <button onClick={ handleUpload }>Detect</button>
+      <Footer></Footer>
     </div>
     );
 }
