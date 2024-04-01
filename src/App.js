4066762d5eefc03528152d1a742ea5e8e95e8a58
Sarthak Mishra <msarthak785@gmail.com>
2024-03-31 20:28:28 -0400

    updated codebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
-import Contact from './Contact';
 import About from './About';
 import Home from './Home';
-import Clock from './Clock';
-import Login from './Login';
-import Signup from './Signup';
 import RealTimeDetection from './RealTimeDetection';
 import UploadSection from './UploadSection';
 
@@ -22,18 +18,6 @@ function App() {
             </Route>
             
             <Route path="/about" element={<About/>}>
-            </Route>
-            
-            <Route path="/contact" element={<Contact/>}>
-            </Route>
-             
-            <Route path="/clock" element={<Clock/>}>
-            </Route>
-
-            <Route path="/login" element={<Login/>}>
-            </Route>
-
-            <Route path="/signup" element={<Signup/>}>
             </Route>
 
             <Route path="/imgDetect" element={<UploadSection/>}>
--- src/Navbar.js
+++ src/Navbar.js
@@ -10,15 +10,7 @@ function Navbar() {
           <Link to="/">Home</Link>
           <Link to="/imgDetect">Image Object Detection</Link>
           <Link to="/liveDetect">Live Detection</Link>
-          <Link to="/contact">Contact</Link>
           <Link to="/about">About Sarthak</Link>
-          <Link to="/clock">Clock</Link>
-        </nav>
-      </div>
-      <div className="Navbar-right">
-        <nav>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup">Sign Up</Link>
         </nav>
       </div>
     </div>
